1e6e3a764c9a8b7f6b6457b3b710082f6c58cca0
arun kumar <Arun@zasyasolutions.com>
2022-08-31 11:53:12 +0530

    remove config file

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('BXC_USER', 'admin@admin.com');
-define('BXC_PASSWORD', '$2y$10$A.4eZ.2gRoEleplNs4icG.82qMJyHgJ4oTnyWxHn6Oyzi6WiBZDoi');
-define('BXC_DB_NAME', 'boxcoin_db');
-define('BXC_DB_USER', 'root');
-define('BXC_DB_PASSWORD', '');
-define('BXC_DB_HOST', 'localhost');
-define('BXC_DB_PORT', '3306');
-define('BXC_URL', 'http://localhost:8000/');
